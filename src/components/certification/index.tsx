4eb92f09c272f25a7fa2f1d15bb3b1caef4df65c
Edson Costa <edson.13.10.2004@gmail.com>
2022-07-05 10:52:29 -0300

    .

--- src/components/certification/index.tsx
+++ src/components/certification/index.tsx
@@ -6,18 +6,18 @@ import { GET_CERTIFICATIONS } from "../../constants/gql";
 export function Certification() {
   const { loading, error, data } = useQuery<{ certifications: CertificationProps[] }>(GET_CERTIFICATIONS);
   if (loading) return (
-    <h1>teste</h1>
+    <h1>Carregando Certificados...</h1>
   )
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error ao buscar Certificados:(</p>;
   return (
-    <aside className="w-full md:w-[348px] bg-gray-700 p-6 border-l border-gray-600">
-      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+    <aside className="w-full md:w-[348px] p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 text-center text-blue-700 block">
         Certificados
       </span>
 
       <div className="flex flex-col gap-8">
       {data?.certifications.map((certification) => {
-            return <h1>{certification.name}</h1>;
+            return <h1 className="text-white text-center">{certification.name}</h1>;
             // <CardCertification
             //   key={certification.id}
             //   title={lesson.title}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 // import './index.css'
-import {Nav} from  './../../components/nav';
-import {About} from './../../components/about';
-import { Certification } from '../../components/certification';
+import {Nav} from  '../../components/Nav';
+import {About} from '../../components/About';
+import { Certification } from '../../components/Certification';
 export function Home() {
 
   return (
